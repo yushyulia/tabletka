5d41ac76fe947a13de49f87a69d9723fc1e33f59
yulia <juliajiyushkova@gmail.com>
2022-04-27 22:26:50 +0700

    аккаунт

--- app/src/main/java/com/example/myapplication/AccountActivity.java
+++ app/src/main/java/com/example/myapplication/AccountActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,16 +10,28 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
 
 public class AccountActivity extends AppCompatActivity {
 
+    TextView text;
     Button exit;
+    FirebaseAuth auth;
+    DatabaseReference users;
+    FirebaseDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
 
+        text = findViewById(R.id.textView);
         exit = findViewById(R.id.exit_btn);
 
         exit.setOnClickListener(new View.OnClickListener() {
@@ -29,5 +42,7 @@ public class AccountActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+
     }
 }
